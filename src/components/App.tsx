c68e4f57cb7711bad9df7b24533080bd0543f89c
catsby <ohndaniel@gmail.com>
2020-04-04 15:23:02 -0700

    Add project.

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "../styles/App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bargin Books</h1>
+      <input></input>
+    </div>
+  )
+}
+
+export default App
