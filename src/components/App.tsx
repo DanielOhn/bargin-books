7a663dd3c4873acc53332ecfbd89dc6e44034bd4
catsby <ohndaniel@gmail.com>
2020-04-04 18:31:21 -0700

    Add autocomplete function for search bar.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,44 @@
-import React from "react"
+import React, { useState } from "react"
 import "../styles/App.css"
 
+let suggested = ["white", "blue", "black", "red", "purple", "yellow", "orange"]
+
 function App() {
+  const [suggest, setSuggest] = useState("")
+
+  const autoComplete = () => {
+    let suggestions = suggested.filter((word) => word.includes(suggest))
+
+    return suggestions
+  }
+
+  const submitVal = () => {
+    console.log("Suggestions: ")
+
+    let words = autoComplete()
+
+    words.map((word) => {
+      console.log(word)
+    })
+  }
+
   return (
     <div className="App">
       <h1>Bargin Books</h1>
-      <input></input>
+      <input
+        id="search"
+        type="text"
+        placeholder="book title"
+        onChange={(e) => setSuggest(e.target.value)}
+      />
+      <button onClick={submitVal}>Search</button>
     </div>
   )
 }
+
+// MAKE AUTOCOMPLETE FOR THE FORM
+// Get a list of suggested words
+// Compare the words to the typed in text in the input
+// Dispaly the array of suggested words that match the currently typed in text
 
 export default App
